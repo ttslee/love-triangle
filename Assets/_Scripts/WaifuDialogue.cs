2b82e2ad4fe9786664fd47fd7a8645b0238d371d
Johnny Ngo <jdngo2@uci.edu>
2020-02-26 22:29:07 -0800

    dialogue script setup

---
+++ Assets/_Scripts/WaifuDialogue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaifuDialogue : MonoBehaviour
+{
+    //public float textSpeed = 0f; //Keep Local Variable syntax in this format*
+    //public int faceExpression = 0; //For future UI effect
+
+
+    void Start() //When the Game Begins, she should say something, so do Say("blah")
+    {
+        //Say("");
+    }
+
+    void Say(string sent) //Implement this to be displayed in UI
+    {
+
+    }
+
+    void Reply(int player, int sent, int expr) //Access this scripts list of replies according to int sent and say it
+    {
+
+    }
+
+
+}
