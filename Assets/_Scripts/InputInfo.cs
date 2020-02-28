dd625b31515408f7c184255effff008fb3648111
ttslee <theodol2@uci.edu>
2020-02-27 23:47:46 -0800

    teo

---
+++ Assets/_Scripts/InputInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputInfo : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+
+    private string action;
+    public string Action
+    {
+        get
+        {
+            return action;
+        }
+        set
+        {
+            action = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
