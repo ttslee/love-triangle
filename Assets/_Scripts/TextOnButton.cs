de8e4d8ca4f59c433f30da21f99b1da734484604
Johnny Ngo <jdngo2@uci.edu>
2020-03-01 03:06:08 -0800

    textwork

---
+++ Assets/_Scripts/TextOnButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class TextOnButton : MonoBehaviour
+{
+
+    TextMeshProUGUI tmpText; //TextMeshPro Text Component
+
+    void Start()
+    {
+        tmpText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        tmpText.transform.localPosition = new Vector3(0, 2, 0);
+        Debug.Log("test");
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(gameObject);
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        tmpText.transform.localPosition = new Vector3(0, 0, 0);
+        Debug.Log("test");
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(null);
+    }
+
+
+
+}
