941e8fff56116aa25911afdf8275fd87b0e9f9b5
Johnny Ngo <jdngo2@uci.edu>
2020-03-02 01:48:57 -0800

    portraits changing

---
+++ Assets/_Scripts/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    List<Sprite> playerIcons = new List<Sprite>();
+    public Image playerLeft;
+    public Image playerRight;
+    private int indexLeft = 0;
+    private int indexRight = 1;
+
+    void Start()
+    {
+        foreach (Sprite s in Resources.LoadAll<Sprite>("Player_Icon"))
+            playerIcons.Add(s); //Loads playerIcons into List
+        playerLeft.sprite = playerIcons[indexLeft];
+    }
+
+    public void ChangeLeftIcon(int dir)
+    {
+        if (dir == -1)
+            playerLeft.sprite = playerIcons[(++indexLeft%playerIcons.Count)];
+        else
+            playerLeft.sprite = playerIcons[(++indexLeft % playerIcons.Count)];
+    }
+
+    public void ChangeRightIcon(int dir)
+    {
+        if (dir == -1)
+            playerRight.sprite = playerIcons[(++indexRight % playerIcons.Count)];
+        else
+            playerRight.sprite = playerIcons[(++indexRight % playerIcons.Count)];
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
